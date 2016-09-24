6a99200f8d8584b7a23da531052761f6a7d7064c
André Dalcin <andre@planosassessoria.com.br>
2016-09-23 21:21:31 -0300

    alteracao para adequar no linux e ruindows

--- main.js
+++ main.js
@@ -3,36 +3,39 @@
 const exec = require('child_process').exec;
 const cpus = require('os').cpus().length;
 const async = require('async');
+const os = require('os');
 
 let host = '172.16.10.47';
 let username = 'operador';
-let source = '/cygdrive/d/aa/';
+let source = './a/';
 let destination = '/Users/operador/teste';
 let timeToSearch = -60;
 let canFind = true;
 
 doFind();
 
-setInterval(() => {
+let pid = setInterval(() => {
     if (canFind) {
         doFind();
     }
 }, (timeToSearch * -1) * 10000);
 
+//console.log(pid);
+
 function doFind() {
     async.queue((task, done) => {
         find((data) => {
             canFind = true;
-            //console.log(data || 'Terminado sem nenhuma alteração no diretório!');
             done();
         })
-    }, cpus).push();
+    }, os.cpus().length).push();
 }
 
 function find(callback) {
     canFind = false;
 
-    exec(`bash --login -c 'find ${source} -cmin ${timeToSearch} -type f'`, (error, stdout, stderr) => {
+    let command = `find ${source} -cmin ${timeToSearch} -type f`;
+    exec((os.platform() == 'win32') ? `C:\\cygwin64\\bin\\bash --login -c ${command}` : command, (error, stdout, stderr) => {
         if (error) {
             console.error(error);
         }
@@ -48,7 +51,8 @@ function find(callback) {
         files = files.join(' ');
 
         if (stdout) {
-            exec(`bash --login -c 'rsync -rvh --ignore-existing ${files} ${username}@${host}:${destination}'`, (error, stdout, stderr) => {
+            let command = `rsync -rvh --ignore-existing ${files} ${username}@${host}:${destination}`;
+            exec((os.platform() == 'win32') ? `C:\\cygwin64\\bin\\bash --login -c ${command}` : command, (error, stdout, stderr) => {
                 if (error) {
                     console.error(error);
                 }

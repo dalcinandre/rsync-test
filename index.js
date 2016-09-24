293c4e20eff51705e678c07c5837d265d7c80350
André Dalcin <andre@planosassessoria.com.br>
2016-09-24 12:21:13 -0300

    atualizações

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ if (cluster.isMaster) {
         console.log(`worker ${worker.process.pid} died`);
     });
 
-    cluster.on('online', function (worker) {
+    /*cluster.on('online', function (worker) {
         console.log('Worker ' + worker.process.pid + ' is online');
-    });
+    });*/
 
     cluster.on('exit', (worker, code, pid) => {
         console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
@@ -25,21 +25,36 @@ if (cluster.isMaster) {
         cluster.fork();
     });
 } else {
-    const Rsync = require('rsync');
-    const chokidar = require('chokidar');
+    const express = require('express');
+    // const http = require('http');
+    const bp = require('body-parser');
+    const finder = require('./src/finder');
 
-    let rsync = new Rsync()
-        .shell('ssh')
-        .flags('avh')
-        .source('./a/*')
-        .destination('./b/');
+    let app = express();
+    app.use(bp.json());
 
-    chokidar.watch('./a', { ignored: /[\/\\]\./ }).on('all', (event, path) => {
-        rsync.execute((error, code, cmd) => {
-            if (error) {
-                console.error(error);
-            }
-            console.log(code, cmd);
-        });
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
     });
+
+    app.get('/time/:time', (req, res) => {
+        finder.setTimeToSearch(req.params.time);
+        return res.status(200).send(`Tempo de busca setado para ${req.params.time}`);
+    });
+
+    app.get('/stop', (req, res) => {
+        try {
+            let t = finder.stop();
+            return res.status(200).send(t || 'Serviço de busca terminado com sucesso!');
+        } catch (error) {
+            return res.status(500).send(error);
+        }
+    });
+
+    let server = app.listen(8080, () => {
+        console.log(`Processo ${process.pid} esta rodando e aceitando conexões na porta ${server.address().port}`);
+    });
+
 }
--- src/finder.js
+++ src/finder.js
@@ -11,23 +11,64 @@ let source = '/cygdrive/d/aa/';
 let destination = '/Users/operador/teste';
 let timeToSearch = -60;
 let canFind = true;
+let pid;
 
-doFind();
+//doFind();
 
-let pid = setInterval(() => {
-    if (canFind) {
-        doFind();
+module.exports.setTimeToSearch = (time) => {
+    this.timeToSearch = time || -60;
+}
+
+module.exports.setHost = (host) => {
+    this.host = host || 'localhost';
+}
+
+module.exports.setUsername = (username) => {
+    this.username = username || 'operador';
+}
+
+module.exports.setSource = (source) => {
+    this.source = source;
+}
+
+module.exports.setDestination = (destination) => {
+    this.destination = destination;
+}
+
+module.exports.start = () => {
+    pid = setInterval(() => {
+        if (canFind) {
+            try {
+                doFind();
+            } catch (error) {
+                throw error;
+            }
+        }
+    }, (timeToSearch * -1) * 10000);
+}
+
+module.exports.stop = () => {
+    if (pid) {
+        try {
+            clearInterval(pid);
+        } catch (error) {
+            throw error;
+        }
+    } else {
+        return 'Nenhum serviço esta rodando!';
     }
-}, (timeToSearch * -1) * 10000);
-
-//console.log(pid);
+}
 
 function doFind() {
     async.queue((task, done) => {
-        find((data) => {
-            canFind = true;
-            done();
-        })
+        try {
+            find((data) => {
+                canFind = true;
+                done();
+            })
+        } catch (error) {
+            throw error;
+        }
     }, os.cpus().length).push();
 }
 
@@ -38,6 +79,7 @@ function find(callback) {
     exec((os.platform() == 'win32') ? `C:\\cygwin64\\bin\\bash --login -c '${command}'` : command, (error, stdout, stderr) => {
         if (error) {
             console.error(error);
+            throw error;
         }
 
         let files = stdout.split('\n');
@@ -55,6 +97,7 @@ function find(callback) {
             exec((os.platform() == 'win32') ? `C:\\cygwin64\\bin\\bash --login -c '${command}'` : command, (error, stdout, stderr) => {
                 if (error) {
                     console.error(error);
+                    throw error;
                 }
 
                 console.log(stdout);
